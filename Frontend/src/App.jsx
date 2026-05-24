a4a586a0454d104d45ef38799fa2d9f332232a82
Aditya <adityasara2622@gmail.com>
2026-05-24 22:05:31 +0530

    login form setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,16 @@
+import { RouterProvider } from "react-router-dom";
+import { router } from "./app.routes";
+
+
+
+
 
 function App() {
 
 
   return (
     <>
-
+      <RouterProvider router={router} />
     </>
   )
 }
--- Frontend/src/app.routes.jsx
+++ Frontend/src/app.routes.jsx
@@ -1,9 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
+import Register from "./features/auth/pages/Register";
+import Login from "./features/auth/pages/Login";
 
 
 
 
 
 
-
-export const router = createBrowserRouter([])
+export const router = createBrowserRouter([
+    {
+        path: "/register",
+        element: <Register />
+    },
+    {
+        path: "/login",
+        element: <Login />
+    }
+])
