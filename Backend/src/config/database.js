bb1569e260aee5a620bd76e31f47e241edb2ffc3
Aditya <adityasara2622@gmail.com>
2026-05-26 17:52:58 +0530

    database issue fix

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -1,14 +1,18 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-
-async function connectToDB(){
+async function connectToDB() {
     try {
-        await mongoose.connect(process.env.MONGO_URI)
+
+        console.log("Mongo URI exists:", !!process.env.MONGO_URI);
+
+        await mongoose.connect(process.env.MONGO_URI);
+
         console.log('Connected to MongoDB');
-    }
-    catch (error) {
-        console.error('Error connecting to MongoDB:', error);
+
+    } catch (error) {
+        console.error('Error connecting to MongoDB:');
+        console.error(error);
     }
 }
 
